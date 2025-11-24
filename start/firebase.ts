6df6823b2100ee38bcd23fa5998d03b2157c79e5
dust1-str <angel.t069@outlook.com>
2025-11-23 19:22:22 -0600

    Rollback

--- start/firebase.ts
+++ start/firebase.ts
@@ -1,9 +1,7 @@
-import fs from 'fs'
 import admin from 'firebase-admin'
 import env from '#start/env'
 
-const path = env.get('FIREBASE_SERVICE_ACCOUNT_PATH')!
-const serviceAccount = JSON.parse(fs.readFileSync(path, 'utf8'))
+const serviceAccount = JSON.parse(env.get('FIREBASE_SERVICE_ACCOUNT')!)
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
