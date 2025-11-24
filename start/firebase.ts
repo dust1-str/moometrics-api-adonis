ccc957cd7a3acbf47c6241c389af8450786f4050
dust1-str <angel.t069@outlook.com>
2025-11-23 19:03:10 -0600

    Holados

--- start/firebase.ts
+++ start/firebase.ts
@@ -1,7 +1,9 @@
+import fs from 'fs'
 import admin from 'firebase-admin'
 import env from '#start/env'
 
-const serviceAccount = JSON.parse(env.get('FIREBASE_SERVICE_ACCOUNT')!)
+const path = env.get('FIREBASE_SERVICE_ACCOUNT_PATH')!
+const serviceAccount = JSON.parse(fs.readFileSync(path, 'utf8'))
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
